2b850e72fc4378ed45a886e4fdac729a6447c7c5
Anne <miss.anne.c@hotmail.com>
2017-04-11 12:10:05 +1200

    Initial Unity Commit

---
+++ Card Engine Tool/Assets/Editor/MyWindow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MyWindow : EditorWindow {
+
+    string myString = "Hi Anne and Amber!";
+    bool groupEnabled;
+    bool myBool = true;
+    float myFloat = 1.23f;
+
+    // Add menu item named "Game Options" to the Window menu
+    [MenuItem("Window/Game Options")]
+
+    public static void ShowWindow()
+    {
+        // Show existing window instance. If one doesn't exist, create one.
+        EditorWindow.GetWindow(typeof(MyWindow));
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Base Settings", EditorStyles.boldLabel);
+        myString = EditorGUILayout.TextField("Text Field", myString);
+
+        groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
+        myBool = EditorGUILayout.Toggle("Toggle", myBool);
+        myFloat = EditorGUILayout.Slider("Slider", myFloat, -3, 3);
+        EditorGUILayout.EndToggleGroup();
+    }
+}
