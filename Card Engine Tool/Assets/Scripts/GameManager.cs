981c164e05913344cc7873be9bca1b4295e6ccd9
Amber <amberleejulian@gmail.com>
2017-04-24 11:14:41 +1200

    Crated varaivkes

--- Card Engine Tool/Assets/Scripts/GameManager.cs
+++ Card Engine Tool/Assets/Scripts/GameManager.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    int typeOfDeck;     //52 Card deck or custom
+
+    //HAND
+    int startingHandSize;
+    int minimumSizeOfHand;
+    int maximumSizeOfHand;
+
+    //DECK
+    int numberOfDecks;
+    int deckSize;
+
+    //TURNS
+    int turnOrder; //NEEDS AN ENUM TO SPECIFY HOW WE PICK TURNS. EG RANDOMIZE, IN ORDER, ETC
+
+    int turnTimer;  //End the turn after this amount of time. 0 for no turn timer.
 }
