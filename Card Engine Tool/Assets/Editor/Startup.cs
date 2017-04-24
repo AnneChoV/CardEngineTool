a0ee6143b507937732746782734a8dfb3d0ca3ee
Anne <miss.anne.c@hotmail.com>
2017-04-24 13:20:49 +1200

    Start Up Window

--- Card Engine Tool/Assets/Editor/Startup.cs
+++ Card Engine Tool/Assets/Editor/Startup.cs
@@ -3,20 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-public class MyWindow : EditorWindow {
+[InitializeOnLoad]
+public class Startup : EditorWindow {
 
-    string myString = "Hi Anne and Amber!";
+    string myString = "Welcome to Card Engine Tool v0.1";
     bool groupEnabled;
     bool myBool = true;
     float myFloat = 1.23f;
+    
+    static Startup()
+    {
+        EditorApplication.update += StartUp;
+    }
+
+    static void StartUp()
+    {
+        EditorApplication.update -= StartUp;
+        ShowWindow();
+    }
 
     // Add menu item named "Game Options" to the Window menu
-    [MenuItem("Window/Game Options")]
+    [MenuItem("Window/Start Up")]
 
     public static void ShowWindow()
     {
         // Show existing window instance. If one doesn't exist, create one.
-        EditorWindow.GetWindow(typeof(MyWindow));
+        EditorWindow.GetWindow(typeof(Startup));
     }
 
     private void OnGUI()
@@ -28,5 +40,10 @@ public class MyWindow : EditorWindow {
         myBool = EditorGUILayout.Toggle("Toggle", myBool);
         myFloat = EditorGUILayout.Slider("Slider", myFloat, -3, 3);
         EditorGUILayout.EndToggleGroup();
+    }
+
+    static void Update()
+    {
+        Debug.Log("Updating");
     }
 }
