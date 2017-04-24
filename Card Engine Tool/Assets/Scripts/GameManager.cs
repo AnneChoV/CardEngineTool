771934cab33caab9a155404d2a250af1b54367b9
Amber <amberleejulian@gmail.com>
2017-04-24 15:13:15 +1200

    Adding and deleting to lists

--- Card Engine Tool/Assets/Scripts/GameManager.cs
+++ Card Engine Tool/Assets/Scripts/GameManager.cs
@@ -4,19 +4,44 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-    int typeOfDeck;     //52 Card deck or custom
+    CardOptions cardOptions;
+    DeckOptions deckOptions;
 
-    //HAND
-    int startingHandSize;
-    int minimumSizeOfHand;
-    int maximumSizeOfHand;
+    //Deck Type
+    public TypeOfDeck DeckType;     //52 Card deck or custom
 
     //DECK
-    int numberOfDecks;
-    int deckSize;
+    public int numberOfDecks;
+    public int deckSize;
+
+    //HAND
+    public int startingHandSize;   
+    public int minimumSizeOfHand;
+    public int maximumSizeOfHand;
 
     //TURNS
-    int turnOrder; //NEEDS AN ENUM TO SPECIFY HOW WE PICK TURNS. EG RANDOMIZE, IN ORDER, ETC
+    public int turnOrder; //NEEDS AN ENUM TO SPECIFY HOW WE PICK TURNS. EG RANDOMIZE, IN ORDER, ETC
+    public int turnTimer;  //End the turn after this amount of time. 0 for no turn timer.
 
-    int turnTimer;  //End the turn after this amount of time. 0 for no turn timer.
+
+    public enum TypeOfDeck
+    {
+        Standard52CardDeck,
+        Custom
+    }
+
+    private void OnValidate()
+    {
+        cardOptions = GetComponent<CardOptions>();
+        deckOptions = GetComponent<DeckOptions>();
+
+        if (DeckType == TypeOfDeck.Custom)
+        {
+           
+        }
+        else
+        {
+
+        }
+    }
 }
